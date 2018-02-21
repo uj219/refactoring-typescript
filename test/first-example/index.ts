894e7824031c2f5b80317a307a14c6712044ef8e
masterpg <masterpg@livedeveloper.net>
2018-02-21 22:23:15 +0900

    first-exampleの単体テストを修正

--- test/first-example/index.ts
+++ test/first-example/index.ts
@@ -6,7 +6,7 @@ suite('first-example', () => {
 
   const movieCommando = new Movie('コマンドー', Movie.REGULAR);
   const movieStandByMe = new Movie('スタンド・バイ・ミー', Movie.REGULAR);
-  const movieBladeRunner2049 = new Movie('ブレードランナー2049', Movie.NEW_RELEASE);
+  const movieYourName = new Movie('君の名は。', Movie.NEW_RELEASE);
 
   const sakuragi = new Customer('桜木 花道');
   const gori = new Customer('赤木 剛憲');
@@ -18,13 +18,14 @@ suite('first-example', () => {
   });
 
   test('statement(): ', () => {
-    // ｢桜木 花道｣が｢コマンドー｣と｢ブレードランナー2049｣をレンタルする
+    // ｢桜木 花道｣が｢コマンドー｣と｢君の名は。｣を｢7日間｣レンタルする
     sakuragi.addRental(new Rental(movieCommando, 7));
-    sakuragi.addRental(new Rental(movieBladeRunner2049, 3));
+    sakuragi.addRental(new Rental(movieYourName, 7));
     // ｢桜木 花道｣がレンタルした明細を取得
     const statement = sakuragi.statement();
+    console.log(`\n\n${statement}\n\n`);
     assert.equal(statement,
-      'Rental Record for 桜木 花道\n\tコマンドー\t9.5\n\tブレードランナー2049\t9\nAmount owed is 18.5\nYou earned 3 frequent renter points');
+      'Rental Record for 桜木 花道\n\tコマンドー\t9.5\n\t君の名は。\t21\nAmount owed is 30.5\nYou earned 3 frequent renter points');
   });
 
 });
